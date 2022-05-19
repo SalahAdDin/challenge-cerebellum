1ee261ad763e395c9f75652986d19cc8d2d0f50a
Luis Alaguna <alagunasalahaddin@live.com>
2022-05-20 00:12:08 +0300

    :sunny: Left aligned text field.

---
+++ src/presentation/widgets/text.styles.ts
@@ -0,0 +1,8 @@
+import { styled, Text } from '@nextui-org/react';
+
+const LeftText = styled(Text, {
+  textTransform: 'capitalize !important',
+  textAlign: 'left',
+});
+
+export default LeftText;
